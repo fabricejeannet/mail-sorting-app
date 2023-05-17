1418f93ce562c3f2b692404710da7c3e392c4622
FisioG <loic.leveque33@gmail.com>
2023-05-17 11:25:12 +0200

    style: :construction: Starting big refactoring

--- match_processor/match_analyser.py
+++ match_processor/match_analyser.py
@@ -2,16 +2,15 @@ from fuzzywuzzy import fuzz
 from match_processor.matching_result import MatchingResult
 from text_processor.text_cleaner import TextCleaner
 from csv_processor.csv_constants import *
+from config_processor.config_importer import ConfigImporter
 
 class MatchAnalyser:
     
     def __init__(self, clients_data_dictionary):
         self.text_cleaner = TextCleaner()
         self.clients_data_dictionary = clients_data_dictionary
-    
-    
-    def get_best_absolute_match_ratio(self,line,searched_string):
-        return max([fuzz.ratio(line, searched_string),fuzz.partial_ratio(line, searched_string),fuzz.token_sort_ratio(line, searched_string),fuzz.token_set_ratio(line, searched_string)])
+        self.config_importer = ConfigImporter()
+        self.threshold = self.config_importer.get_image_minimum_threshold()
     
     
     def get_match_ratio(self,line,searched_string):
@@ -32,35 +31,43 @@ class MatchAnalyser:
         return int((self.get_match_ratio(line,searched_string) + self.get_match_ratio_for_names(line,searched_string)) / 2)
     
     
-    def return_the_top_three_matches_for_a_line(self,line):
-        result_list = self.add_matching_company_names_to_result_list([], line)
-        result_list.sort(key=lambda x: x.correspondance_ratio, reverse=True)
-        if result_list == [] or result_list[0].correspondance_ratio <= 90:
-            result_list = self.add_matching_directors_names_to_result_list(result_list, line)
-            result_list.sort(key=lambda x: x.correspondance_ratio, reverse=True)
+    def return_the_top_three_matches_for_a_line(self, line):
+        result_list = self.get_matching_companies(line)
+        result_list.sort(key=lambda x: x.match_ratio, reverse=True)
+        if result_list == [] or result_list[0].match_ratio <= 90:
+            result_list += self.get_matching_directors_names(line)
+            result_list.sort(key=lambda x: x.match_ratio, reverse=True)
         return result_list[:3]
     
     
-    def add_matching_company_names_to_result_list(self, result_list, line):
-        all_research_fields_for_company = [self.clients_data_dictionary[COMPANY_NAME],self.clients_data_dictionary[TRADEMARK_NAME]]
-        for research_field in all_research_fields_for_company:
-            for index in range(len(research_field)):
-                company_name = str(research_field[index]).lower().strip()
-                company_name = self.text_cleaner.remove_legal_status(company_name)
-                correspondance_ratio = self.get_match_ratio_for_company_names(line,company_name)
-                if (correspondance_ratio > THRESHOLD):
-                    matching_result = MatchingResult(company_name, correspondance_ratio, self.clients_data_dictionary[STATUS][index])
-                    result_list.append(matching_result)
-        return result_list
+    def get_matching_companies(self, line):
+        match_list = []
+        companies = self.clients_data_dictionary[COMPANY_NAME]
+        trade_marks = self.clients_data_dictionary[TRADEMARK_NAME]
+      
+        for index in range(len(companies)):
+            company_name = self.text_cleaner.remove_legal_status(companies[index].lower().strip())
+            trade_mark = self.text_cleaner.remove_legal_status(trade_marks[index].lower().strip())
+            
+            company_name_match_ratio = self.get_match_ratio_for_company_names(line,company_name)
+            trade_mark_match_ratio = self.get_match_ratio_for_company_names(line,trade_mark)
+            
+            if (company_name_match_ratio > self.threshold):         
+                match_list.append(MatchingResult(company_name, company_name_match_ratio, self.clients_data_dictionary[STATUS][index]))
+            elif (trade_mark_match_ratio > self.threshold):
+                match_list.append(MatchingResult(trade_mark, trade_mark_match_ratio, self.clients_data_dictionary[STATUS][index]))
+                
+        return match_list
 
 
-    def add_matching_directors_names_to_result_list(self, result_list, line):
+    def get_matching_directors_names(self, line):
+        result_list = []
         all_research_fields_for_directors = [self.clients_data_dictionary[DIRECTOR_NAME],self.clients_data_dictionary[LEGAL_REPRESENTATIVE]]
         for research_field in all_research_fields_for_directors:
             for index in range(len(research_field)):
                 director_name = str(research_field[index]).lower().strip()
                 correspondance_ratio = self.get_match_ratio_for_names(line,director_name)
-                if (correspondance_ratio > THRESHOLD):
+                if (correspondance_ratio > self.threshold):
                     matching_result = MatchingResult(director_name, correspondance_ratio, self.clients_data_dictionary[STATUS][index])
                     result_list.append(matching_result)
         return result_list
--- streetfacteur_processor/app.py
+++ streetfacteur_processor/app.py
@@ -7,16 +7,17 @@ import time
 from config_processor.config_importer import ConfigImporter
 
 
-street_facteur_gui = StreetFacteur()
 config_importer = ConfigImporter()
+
+street_facteur_gui = StreetFacteur()
 observer = Observer()
 event_handler = CsvHandler(street_facteur_gui)
 observer.schedule(event_handler, path=config_importer.get_csv_file_path(), recursive=False)
 observer.start()
 street_facteur_gui.main()
 # run the observer infinitely
-try:
-    while True:
-        time.sleep(10)
-except KeyboardInterrupt:
-    observer.stop()
+# try:
+#     while True:
+#         time.sleep(10)
+# except KeyboardInterrupt:
+#     observer.stop()
