ebd3bad8a0ecfe02fc1a4ea5a5260cccd64d238d
fabricejeannet <fabricejeannet@gmail.com>
2023-06-30 11:16:47 +0200

    Edition du fichier de configuration pour la nouvelle carte SD Création du script bash de lancement Ajout d'une icone Ajout de SARLU aux status legaux

---
+++ msa.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ~/mail-sorting-app
+python app.py
+
--- text_processor/text_constants.py
+++ text_processor/text_constants.py
@@ -3,6 +3,7 @@ REGEX_BRACKET = r"\[|\]"
 REGEX_CONSECUTIVE_NUMBERS = r'\d\s*\d\s*\d\s*\d\s*\d\s*\d(?:\s*\d)*'
 REGEX_SAS = r'(?<!\w)(?:\(?[sS]\.?[aA]\.?[sS]\b\.?\)?)(?!\w)'
 REGEX_SARL = r'(?<!\w)(?:\(?[sS]\.?[aA]\.?[rR]\.?[lL]\b\.?\)?)(?!\w)'
+REGEX_SARLU = r'(?<!\w)(?:\(?[sS]\.?[aA]\.?[rR]\.?[lL]\.?[uU]\b\.?\)?)(?!\w)'
 REGEX_SASU = r'(?<!\w)(?:\(?[sS]\.?[aA]\.?[sS]\.?[uU]\b\.?\)?)(?!\w)'
 REGEX_EURL = r'(?<!\w)(?:\(?[eE]\.?[uU]\.?[rR]\.?[lL]\b\.?\)?)(?!\w)'
 REGEX_SCI = r'(?<!\w)(?:\(?[sS]\.?[cC]\.?[iI]\b\.?\)?)(?!\w)'
@@ -20,4 +21,4 @@ REGEX_GENDER_MARKERS = r'\b(m\b\.?|mme\b\.?|mr\b\.?|madame\b|monsieur\b|mmemr\b|
 BANNED_WORDS_LIST = ["33000 bordeaux", "recommande","9 rue de conde bureau 3", "societe en formation", "3eme etage","le directeur","la directrice","france","adresse de livraison", "le representant legal","9 r de condé", "9 rue de condé" ,"coolworking","representant legal", "le dirigeant de l'entreprise" "9 r de conde", "rue de conde","9 rue conde","titulaire du compte", "representant legal", "retour à", "a l'attention du dirigeant" ,"bureau 3", "destinataire", "ecopli", "etage 3", "numero de ", "\\x0c"]
 
 # Put the substrings regex at the end of the list to avoid matching a substring before the full string
-LEGAL_STATUS = [REGEX_SARL, REGEX_SASU, REGEX_EURL, REGEX_SCI, REGEX_SNC,REGEX_GROUPE, REGEX_EIRL, REGEX_CABINET, REGEX_STE, REGEX_SOCIETE, REGEX_ENTREPRISE, REGEX_SAS, REGEX_EI, REGEX_CIE]
+LEGAL_STATUS = [REGEX_SARLU, REGEX_SARL, REGEX_SASU, REGEX_EURL, REGEX_SCI, REGEX_SNC,REGEX_GROUPE, REGEX_EIRL, REGEX_CABINET, REGEX_STE, REGEX_SOCIETE, REGEX_ENTREPRISE, REGEX_SAS, REGEX_EI, REGEX_CIE]
